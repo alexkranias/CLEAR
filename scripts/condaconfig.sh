01fb6edaee93ebb8ba05fb7e0669e53ce2b1a42e
alexkranias <alexander.kranias@gmail.com>
2025-03-28 09:45:53 -0400

    fix: conda loads in scratch

--- scripts/condaconfig.sh
+++ scripts/condaconfig.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+mkdir -p /storage/scratch1/4/akranias3/.conda/envs
+mkdir -p /storage/scratch1/4/akranias3/.conda/pkgs
+
 # Load SLURM Modules
 module load anaconda3
 
 # Configure Conda to use research group storage
-conda config --add envs_dirs /storage/coda1/p-hshi308/0/akranias3/.conda/envs
-conda config --add pkgs_dirs /storage/coda1/p-hshi308/0/akranias3/.conda/pkgs
+conda config --add envs_dirs /storage/scratch1/4/akranias3/.conda/envs
+conda config --add pkgs_dirs /storage/scratch1/4/akranias3/.conda/pkgs
